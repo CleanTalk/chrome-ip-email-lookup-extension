3f6a41f94b43e4c40a0d1a3c1aecd9ba71b9269f
githubfan86 <network_leader@outlook.com>
2022-04-20 23:35:26 +0800

    first commit

---
+++ background.js
@@ -0,0 +1,49 @@
+chrome.contextMenus.create({
+    id: "cleanTalk",
+    title: "cleantalk.org lookup",
+    contexts: ["selection"]
+}, () => chrome.runtime.lastError);
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+    if (clickData.menuItemId == "cleanTalk" && clickData.selectionText) {
+        var is_valid = 0;
+        const selected_text = clickData.selectionText.trim();
+        if ( selected_text.indexOf('@') > -1 ) {
+            if ( validateEmail(selected_text) )
+                is_valid = 1;
+        }
+        else {
+            if ( validateIPAddress(selected_text) )
+                is_valid = 1;
+        }
+
+        if ( is_valid ) {
+            var url = 'https://cleantalk.org/blacklists/' + selected_text;
+            chrome.tabs.create({ url: url });
+        }
+        else {
+            notification('Lookup does work only for valid IP or Email address');
+        }
+    }
+});
+
+function notification(message) {
+    chrome.notifications.create({
+        title: 'CleanTalk',
+        message: message,
+        iconUrl: 'images/cleantalk48.png',
+        type: 'basic'
+    })
+}
+
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+const validateIPAddress = (ip) => {     // Support IPv4 and IPv6
+    return ip.match(
+        /(?:^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$)|(?:^(?:(?:[a-fA-F\d]{1,4}:){7}(?:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,2}|:)|(?:[a-fA-F\d]{1,4}:){4}(?:(?::[a-fA-F\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,3}|:)|(?:[a-fA-F\d]{1,4}:){3}(?:(?::[a-fA-F\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,4}|:)|(?:[a-fA-F\d]{1,4}:){2}(?:(?::[a-fA-F\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,5}|:)|(?:[a-fA-F\d]{1,4}:){1}(?:(?::[a-fA-F\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,6}|:)|(?::(?:(?::[a-fA-F\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,7}|:)))(?:%[0-9a-zA-Z]{1,})?$)/gm
+    );
+};
